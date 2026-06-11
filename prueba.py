b43e6e1e66886c1e860b1ba304f0d1a8abe5fd2e
grupodellossomalugani <grupo.dellossomalugani@gmail.com>
2026-06-11 09:38:49 +0000

    diseno de datos y explicacion de open() y sus argumentos

--- prueba.py
+++ prueba.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import csv 
+import codecs
 
 # LIBRERIAS Q SE PUEDEN USAR StreamLight.io, MatPlotLib, csv y Pytest 
 
@@ -13,6 +14,27 @@ import csv
 
 # Para ejecutar el codigo " python3 -m streamlit run prueba.py " ==========
 
-st.title("Proyecto programacion 2 ")
-st.write("hol")
-#hola prueba de commit
+#st.title("Proyecto programacion 2 ")
+#st.subheader("Airbnb, ciudad de México DF (MEX)")
+
+"""DATASET = open("DATASET.csv", "r",encoding="UTF-8")
+for i in DATASET:
+    print(i)
+DATASET.close()"""
+
+
+# La funcion open( nombre_de_archivo , modo , encoding=None) tiene 3 argumentos los cuales 
+#  --  nombre_de_archivo: es el nombre del archivo
+#  --  modo: puede ser 'r' cuando el fichero solo se lea
+#                    'w' para solo escritura
+#                    'a' abre el fichero para agregar cualquier dato
+#                    'r+' abre el fichero tanto para lectura como para escritura
+#                    (el argumento mode es opcional, se asume 'r' si se omite)
+#  -- encoding="utf-8": es la codificacion del archivo incluye todo el alfebeto en distintos idiomas y emojis 
+
+f = open("DATASET.csv",'w',encoding="utf-8")
+
+with open("DATASET.csv",encoding="utf-8") as f:
+    read_data = f.read()
+
+f.closed

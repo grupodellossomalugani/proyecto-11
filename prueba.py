b3ee43050a4bb4cb2086ba17ab70c6bf9e133ead
grupodellossomalugani <grupo.dellossomalugani@gmail.com>
2026-06-11 08:28:14 +0000

    prueba

--- prueba.py
+++ prueba.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import csv 
+
+# LIBRERIAS Q SE PUEDEN USAR StreamLight.io, MatPlotLib, csv y Pytest 
+
+# streamlit = crea paginas web interactivas traduciendo todo lo que se hace en python en (HTML, CSS y JavaScript)
+
+# MatPlotLib = crea graficos para mostrar datos 
+
+# csv = permite leer y escribir datos en archivos CSV (valores Separados por Comas)
+
+# Pytest = para crear casos de pruebas
+
+# Para ejecutar el codigo " python3 -m streamlit run prueba.py " ==========
 
 st.title("Proyecto programacion 2 ")
 st.write("hol")

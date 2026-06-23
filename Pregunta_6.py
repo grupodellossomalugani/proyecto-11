accf08fc65fdb24f35b7eae37270c6e34e53140b
grupodellossomalugani <grupo.dellossomalugani@gmail.com>
2026-06-23 20:17:17 +0000

    Se agrego paramentros a la funcion y un caso de prueba

--- Pregunta_6.py
+++ Pregunta_6.py
@@ -2,9 +2,8 @@ from lector import dataset
 import streamlit as st
 
 
-def PREGUNTA_6():
+def PREGUNTA_6(DATOS)->list:
     
-    DATOS = dataset()
 
 
     # Coordenadas Catedral
@@ -35,14 +34,21 @@ def PREGUNTA_6():
                 Distancia = ( ( (X-Xc)**2 + (Y-Yc)**2 ) ** 0.5) * 108
                 Distancia = round(Distancia,1)
 
-
+                if fila["price"] != "":
                 # Agrega los datos que satisfacen las condiciones
-                tabla.append({
-                    "Nombre": fila["name"],
-                    "Distancia(Km)": Distancia,
-                    "Precion": fila["price"],
-                    "Direccion": " "
-                })
+                    tabla.append({
+                        "Nombre": fila["name"],
+                        "Distancia(Km)": Distancia,
+                        "Precio": float(fila["price"]),
+                        "Direccion": " "
+                    })
+                else:
+                    tabla.append({
+                        "Nombre": fila["name"],
+                        "Distancia(Km)": Distancia,
+                        "Precio": fila["price"],
+                        "Direccion": " "
+                    })
 
 
     # Ordena la tabla por orden ASC de las distancias
@@ -51,5 +57,43 @@ def PREGUNTA_6():
     # Tabla
     st.dataframe(tabla)
 
+    return tabla
 
+
+
+def test_PREGUNTA_6():
+    
+    datos_test = [{
+        "name": "Hotel Catedral",
+        "longitude": "-99.13306",
+        "latitude": "19.43444",
+        "price": 500
+    },
+    {
+        "name": "Hotel Cercano",
+        "longitude": "-99.13250",
+        "latitude": "19.43400",
+        "price": 800
+    },
+    {
+        "name": "Hotel Lejano",
+        "longitude": "-99.12000",
+        "latitude": "19.42000",
+        "price": 1200
+    }]
+    
+    resultado = [{
+        "Nombre": "Hotel Catedral",
+        "Distancia(Km)": 0.0,
+        "Precio": 500,
+        "Direccion": " "
+    },
+    {
+        "Nombre": "Hotel Cercano",
+        "Distancia(Km)": 0.1,
+        "Precio": 800,
+        "Direccion": " "
+    }]
+
+    assert PREGUNTA_6(datos_test) == resultado
 

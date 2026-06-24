1ca6ec62c3ed8cfa2bd3618e89ede41e19fc8e87
grupodellossomalugani <grupo.dellossomalugani@gmail.com>
2026-06-24 14:01:26 +0000

    Signaura y declaración de proposito

--- Pregunta_6.py
+++ Pregunta_6.py
@@ -2,8 +2,18 @@ from lector import dataset
 import streamlit as st
 
 
-def PREGUNTA_6(DATOS)->list:
-    
+def PREGUNTA_6(DATOS:list)->list:
+    """
+    La función recibe la dataset en forma de lista, esta calcula los hospedajes que
+    se ubican dentro de un radio de 0.5 km de distancia de la Catedral de Ciudad de México
+    Ejemplo:
+    PREGUNTA_6([{"name": "Hotel Catedral", "longitude": "-99.13306", "latitude": "19.43444", "price": 500},
+                { "name": "Hotel Cercano", "longitude": "-99.13250", "latitude": "19.43400", "price": 800},
+                {"name": "Hotel Lejano", "longitude": "-99.12000", "latitude": "19.42000", "price": 1200}]) ==
+            
+            [{"Nombre": "Hotel Catedral", "Distancia(Km)": 0.0, "Precio": 500, "Ubicación": (-99.13306, 19.43444)},
+             {"Nombre": "Hotel Cercano", "Distancia(Km)": 0.1, "Precio": 800, "Ubicación": (-99.13250,19.43400)}]
+    """
 
 
     # Coordenadas Catedral
@@ -11,9 +21,11 @@ def PREGUNTA_6(DATOS)->list:
     Yc = 19.43444
 
 
+    # Tabla resultante
     tabla = []
 
 
+    # Lectura del dataset
     for fila in DATOS:
 
 
@@ -34,21 +46,13 @@ def PREGUNTA_6(DATOS)->list:
                 Distancia = ( ( (X-Xc)**2 + (Y-Yc)**2 ) ** 0.5) * 108
                 Distancia = round(Distancia,1)
 
-                if fila["price"] != "":
                 # Agrega los datos que satisfacen las condiciones
-                    tabla.append({
-                        "Nombre": fila["name"],
-                        "Distancia(Km)": Distancia,
-                        "Precio": float(fila["price"]),
-                        "Direccion": " "
-                    })
-                else:
-                    tabla.append({
-                        "Nombre": fila["name"],
-                        "Distancia(Km)": Distancia,
-                        "Precio": fila["price"],
-                        "Direccion": " "
-                    })
+                tabla.append({
+                    "Nombre": fila["name"],
+                    "Distancia(Km)": Distancia,
+                    "Precio": fila["price"],
+                    "Ubicación": (X,Y)
+                })
 
 
     # Ordena la tabla por orden ASC de las distancias
@@ -86,13 +90,13 @@ def test_PREGUNTA_6():
         "Nombre": "Hotel Catedral",
         "Distancia(Km)": 0.0,
         "Precio": 500,
-        "Direccion": " "
+        "Ubicación": (-99.13306, 19.43444)
     },
     {
         "Nombre": "Hotel Cercano",
         "Distancia(Km)": 0.1,
         "Precio": 800,
-        "Direccion": " "
+        "Ubicación":(-99.13250,19.43400)
     }]
 
     assert PREGUNTA_6(datos_test) == resultado

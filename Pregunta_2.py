2926b9be616e88c1f2b8b9eb7014b161369a635d
grupodellossomalugani <grupo.dellossomalugani@gmail.com>
2026-06-24 20:42:48 +0000

    creacion de la funcion que responde a la pregunta 2

--- Pregunta_2.py
+++ Pregunta_2.py
@@ -1,5 +1,47 @@
-
 #Tomas
 
 #Cosas por hacer 
 #Hacer todo el codigo + el diseno de datos y implementarlo en el dashboard 
+
+from lector import dataset
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def PREGUNTA_2(alcaldia):
+
+    DATASET = dataset()
+
+    habitaciones = {}
+
+    for fila in DATASET:
+        if fila["neighbourhood"] == alcaldia:
+
+            tipo = fila["room_type"]
+
+            if tipo not in habitaciones:
+
+                habitaciones[tipo] = 1
+
+            else:
+
+                habitaciones[tipo] += 1
+
+    etiquetas = []
+    cantidades = []
+
+    for tipo in habitaciones:
+
+        etiquetas.append(tipo)
+        cantidades.append(habitaciones[tipo])
+    
+    fig, ax = plt.subplots()
+
+    ax.pie(
+        cantidades,labels = etiquetas, autopct="%1.1f%%"
+    )
+
+    ax.set_title(
+        "Distribucion de tipos de habitaciones en " + alcaldia 
+    )
+
+    st.pyplot(fig)
